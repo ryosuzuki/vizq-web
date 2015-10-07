f5950d5e4bc94352f02486714697bddad38cb9d4
Ryo Suzuki <ryosuzk@gmail.com>
2015-10-07 00:20:30 -0600

    Tableau like grouped bar chart

--- js/main.js
+++ js/main.js
@@ -1,3 +1,3 @@
-window.data = require('../node_modules/vizq/data/fcq.5000.json');
+window.json = require('../node_modules/vizq/data/fcq.5000.json');
 window.vizq = require('../node_modules/vizq/lib/index.js')();
 window.hanson = require('hanson');
--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-vizq.load(data.slice(0,1000))
+vizq.load(json.slice(0,1000))
 
 var result;
 var editor = ace.edit("editor");
@@ -6,7 +6,7 @@ var text = "{\n  Subject: 'col',\n  CrsLvlNum: 'col'\n}"
 editor.setValue(text);
 
 
-$('#data').text(JSON.stringify(window.data, null, 2));
+$('#data').text(JSON.stringify(window.json, null, 2));
 
 $('#viz').click( function () {
   var text = editor.getValue();
@@ -25,17 +25,19 @@ $('.menu .item').tab();
 
 
 var margin = {top: 20, right: 20, bottom: 130, left: 40},
-width = 500 - margin.left - margin.right,
+width = 1500 - margin.left - margin.right,
 height = 500 - margin.top - margin.bottom;
 
-var x = d3.scale.ordinal()
+var x0 = d3.scale.ordinal()
 .rangeRoundBands([0, width], .1);
+
+var x1 = d3.scale.ordinal();
 
 var y = d3.scale.linear()
 .range([height, 0]);
 
 var xAxis = d3.svg.axis()
-.scale(x)
+.scale(x0)
 .orient("bottom");
 
 var yAxis = d3.svg.axis()
@@ -43,7 +45,9 @@ var yAxis = d3.svg.axis()
 .orient("left")
 .ticks(10);
 
-var color = d3.scale.category20b();
+// var color = d3.scale.category20b();
+var color = d3.scale.ordinal()
+  .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
 
 var svg = d3.select("#canvas").append("svg")
 .attr("width", width + margin.left + margin.right)
@@ -52,18 +56,34 @@ var svg = d3.select("#canvas").append("svg")
 .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 var xLabel, yLabel, bars;
+var state, legend;
 var svg2, childre;
 
 function visualize () {
 
   if (xLabel) xLabel.remove();
   if (yLabel) yLabel.remove();
+  if (state) state.remove();
+  if (legend) legend.remove();
   if (bars) bars.remove();
   if (svg2) svg2.remove();
 
   var name = Object.keys(result[0])[0];
-  x.domain(result.map(function(d) { return d[name]; }));
-  y.domain([0, d3.max(result, function(d) { return d.count; })]);
+  var childrenName = Object.keys(result[0].children[0])[0];
+
+  var childrenNames = [];
+  result.forEach( function (d) {
+    var names = d.children.map( function (child) {
+      return child[childrenName];
+    })
+    childrenNames = _.uniq(childrenNames.concat(names)).sort();
+  })
+
+  x0.domain(result.map(function (d) { return d[name]; }));
+  x1.domain(childrenNames).rangeRoundBands([0, x0.rangeBand()])
+  y.domain([0, d3.max(result, function (d) {
+    return d3.max(d.children, function (d) { return d.count; })
+  })]);
 
   xLabel = svg.append("g")
   .attr("class", "x axis")
@@ -87,8 +107,44 @@ function visualize () {
   .style("text-anchor", "end")
   .text("Frequency");
 
-  bars = svg.selectAll(".bar")
+  state = svg.selectAll('.state')
   .data(result)
+  .enter().append('g')
+  .attr('class', 'g')
+  .attr('transform', function (d) { return 'translate(' + x0(d[name]) + ',0)'; });
+
+  state.selectAll('rect')
+  .data(function (d) { return d.children; })
+  .enter().append('rect')
+  .attr('width', x1.rangeBand())
+  .attr('x', function (d) { return x1(d[childrenName]); })
+  .attr('y', function (d) { return y(d.count); })
+  .attr('height', function (d) { return height - y(d.count); })
+  .style('fill', function (d) { return color(d[childrenName]); });
+
+  legend = svg.selectAll(".legend")
+  .data(childrenNames.slice().reverse())
+  .enter().append("g")
+  .attr("class", "legend")
+  .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
+
+  legend.append("rect")
+  .attr("x", 100 - 18)
+  .attr("width", 18)
+  .attr("height", 18)
+  .style("fill", color);
+
+  legend.append("text")
+  .attr("x", 100 - 24)
+  .attr("y", 9)
+  .attr("dy", ".35em")
+  .style("text-anchor", "end")
+  .text(function(d) { return d; });
+
+
+/*
+  bars = svg.selectAll(".bar")
+  .data(data)
   .enter().append("rect")
   .attr("class", "bar")
   .attr("x", function(d) { return x(d[name]); })
@@ -151,6 +207,7 @@ function visualize () {
 
     // visualize();
   }
+  */
 
   function type(d) {
     d.frequency = +d.frequency;
